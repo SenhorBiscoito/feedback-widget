05bbab351e55a9456b2d9cc4fc8d16e41917d148
Senhor Biscoito <mirandajean2009@gmail.com>
2022-05-02 15:13:23 -0300

    Criando component de Widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-function App() {
-  return <h1 className="bg-cyan-800 p-1 h-10 rounded">Hello World</h1>;
-}
+import { Widget } from "./components/Widget";
 
-export default App;
+export function App() {
+  return <Widget />;
+}
