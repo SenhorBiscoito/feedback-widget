0acaf6c0c6a4110b196c8917d83f9860114f59f7
Senhor Biscoito <mirandajean2009@gmail.com>
2022-05-05 12:34:27 -0300

    Criando rota de feedbacks

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,23 @@
 import express from "express";
+import { prisma } from "./prisma";
 
 const app = express();
 
+app.use(express.json());
+
+app.post("/feedbacks", async (request, response) => {
+  const { type, comment, screenshot } = request.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return response.status(201).json({ data: feedback });
+});
 
 app.listen(3333, () => {
   console.log("Server started on port 3333");
