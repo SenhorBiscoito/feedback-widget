a41169b3b155c5eea42e940b541fb545e73676c2
Senhor Biscoito <mirandajean2009@gmail.com>
2022-05-05 17:15:15 -0300

    Snippets para criação de componentes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+
+import { theme } from "./src/theme";
+import { Widget } from "./src/components/Widget/index ";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+      }}
+    >
+      <Widget />
+      <StatusBar backgroundColor="transparent" style="light" translucent />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
